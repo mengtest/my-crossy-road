05f2fbcd7832cf88ada25e903157e5c679b9f162
Agung Baptiso Sorlawan <agungbsorlawan@gmail.com>
2016-04-10 10:37:07 +0700

    create prototype for crossy-road
    create player game object
    script for jump and bounce
    configure the state machine animation

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+
+	Animator anim;
+	public GameObject thePlayer;
+
+	void Start () 
+	{
+		anim = gameObject.GetComponent<Animator> ();
+	}
+	
+	void Update () 
+	{
+		Bounce bounceScript = thePlayer.GetComponent<Bounce> ();
+		if (bounceScript.justJump == true) 
+		{
+			anim.SetBool ("Jump", true);
+		} 
+		else 
+		{
+			anim.SetBool ("Jump", false);
+		}
+	}
+}
---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounce : MonoBehaviour {
+
+	float lerpTime;
+	float currentLerpTime;
+	float perc = 1;
+
+
+	Vector3 startPos;
+	Vector3 endPos;
+
+	bool firstInput;
+	public bool justJump;
+
+	void Update () 
+	{
+		if (Input.GetButtonDown("up") || Input.GetButtonDown("down") || Input.GetButtonDown("right") || Input.GetButtonDown("left"))
+		{
+			if (perc == 1) 
+			{
+				lerpTime = 1;
+				currentLerpTime = 0;
+				firstInput = true;
+				justJump = true;
+			}
+		}
+		startPos = gameObject.transform.position;
+
+		if (Input.GetButtonDown("right") && gameObject.transform.position == endPos)
+		{
+			endPos = new Vector3 (transform.position.x + 1, transform.position.y, transform.position.z);
+		}
+		if (Input.GetButtonDown("left") && gameObject.transform.position == endPos)
+		{
+			endPos = new Vector3 (transform.position.x - 1, transform.position.y, transform.position.z);
+		}
+		if (Input.GetButtonDown("up") && gameObject.transform.position == endPos)
+		{
+			endPos = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 1);
+		}
+		if (Input.GetButtonDown("down") && gameObject.transform.position == endPos)
+		{
+			endPos = new Vector3 (transform.position.x, transform.position.y, transform.position.z - 1);
+		}
+
+		if (firstInput == true) 
+		{
+			currentLerpTime += Time.deltaTime * 5;
+			perc = currentLerpTime / lerpTime;
+			gameObject.transform.position = Vector3.Lerp (startPos, endPos, perc); 
+			if (perc > 0.8) 
+			{
+				perc = 1;
+			}
+
+			if (Mathf.Round (perc) == 1) 
+			{
+				justJump = false;
+			}
+		}
+	}
+}
