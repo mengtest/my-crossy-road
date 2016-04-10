410c4a5f168802d90b7c23b00ff39bbd6a0f6fba
Agung Baptiso Sorlawan <agungbsorlawan@gmail.com>
2016-04-11 01:16:06 +0700

    make camera to follow the player / Chicken

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject chickenPlayer;
+	Vector3 shouldPos;
+
+	void Update () {
+		shouldPos = Vector3.Lerp (gameObject.transform.position, chickenPlayer.transform.position, Time.deltaTime);
+		gameObject.transform.position = new Vector3 (shouldPos.x, 1, shouldPos.z); 
+	}
+}
