bd2eae68f1943ef4ea3a41a34b0ad66efa1d1d79
Agung Baptiso Sorlawan <agungbsorlawan@gmail.com>
2016-04-11 01:09:14 +0700

    make chicken to turn based on the move direction

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -22,5 +22,23 @@ public class AnimationController : MonoBehaviour {
 		{
 			anim.SetBool ("Jump", false);
 		}
+
+
+		if (Input.GetButtonDown("right"))
+		{
+			gameObject.transform.rotation = Quaternion.Euler (0, 180, 0);	
+		}
+		if (Input.GetButtonDown("left"))
+		{
+			gameObject.transform.rotation = Quaternion.Euler (0, 0, 0);	
+		}
+		if (Input.GetButtonDown("up"))
+		{
+			gameObject.transform.rotation = Quaternion.Euler (0, 90, 0);	
+		}
+		if (Input.GetButtonDown("down"))
+		{
+			gameObject.transform.rotation = Quaternion.Euler (0, 270, 0);	
+		}
 	}
 }
